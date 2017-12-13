f0d1cab026006275f2ee04a686dd5592b374db9c
LeChunlin <lechunlin@simuwang.com>
2017-12-13 18:19:21 +0800

    sdfs

--- t.sh
+++ t.sh
@@ -1,18 +1,24 @@
-#!/bin/bash
+#!/bin/sh
 
 git add . 
-echo '请输入commit:'
+echo '-------请输入commit:'
 read commitText
-echo 'commit信息是: ${commitText}'
-git commit -m ${commitText}
+echo "-------commit信息是: $commitText-------"
+git commit -m${commitText}
+echo "------------------commit------------------"
 git pull origin master
 git checkout le
-echo '切换到le分支'
+echo '-------切换到le分支-------'
+git add . 
+echo '-------请输入le分支 commit:'
+read commitTextLe
+echo "-------commit信息是: $commitTextLe-------"
+git commit -m${commitTextLe}
 git merge fix
-echo '合并fix分支'
+echo '-------合并fix分支-------'
 git push origin le
-echo '推送到le分支'
+echo '-------推送到le分支-------'
 git checkout fix
-echo '切换到fix分支'
+echo '-------切换到fix分支-------'
 
 
